8c5854b2bd77c4e98b3ecb75a93c437ab6c3c395
MON\monar <hieunt0220@gmail.com>
2024-01-05 16:42:41 +0700

    WIP

---
+++ services/urql/exchanges.ts
@@ -0,0 +1,19 @@
+import { createClient as createWSClient } from "graphql-ws"
+import { subscriptionExchange as se } from "urql"
+import GraphqlConfig from "common/graphql-config"
+
+const wsClient = createWSClient({
+  url: GraphqlConfig.wsUrl,
+})
+
+export const subscriptionExchange = se({
+  forwardSubscription(request) {
+    const input = { ...request, query: request.query || "" }
+    return {
+      subscribe(sink) {
+        const unsubscribe = wsClient.subscribe(input, sink)
+        return { unsubscribe }
+      },
+    }
+  },
+})
---
+++ services/urql/useURQLClient.ts
@@ -0,0 +1,37 @@
+import { requestPolicyExchange } from "@urql/exchange-request-policy"
+import { retryExchange } from "@urql/exchange-retry"
+import { useMemo } from "react"
+import { cacheExchange, createClient, fetchExchange, ssrExchange } from "urql"
+import GraphqlConfig from "common/graphql-config"
+import { subscriptionExchange } from "./exchanges"
+
+export default function useURQLClient() {
+  const [client, ssr] = useMemo(() => {
+    const ssr = ssrExchange()
+    const client = createClient({
+      url: GraphqlConfig.url,
+      exchanges: [
+        requestPolicyExchange({
+          ttl: 60 * 1000,
+          shouldUpgrade: (operation) => operation.context.requestPolicy !== "cache-only",
+        }),
+        cacheExchange,
+        ssr,
+        retryExchange({
+          initialDelayMs: GraphqlConfig.initialDelayMs,
+          maxDelayMs: GraphqlConfig.maxDelayMs,
+          randomDelay: true,
+          maxNumberAttempts: 2,
+          retryIf: (err) => !!(err && err.networkError),
+        }),
+        fetchExchange,
+        subscriptionExchange,
+      ],
+      suspense: true,
+    })
+
+    return [client, ssr]
+  }, [])
+
+  return [client, ssr]
+}
